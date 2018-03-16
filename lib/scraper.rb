0e419872d3de8a6d8fae5ebc2a320f68c5144d42
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-16 01:58:18 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,14 +9,13 @@ class Scraper
     
   end
   
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
+  def print_courses
+  self.make_courses
+  Course.all.each do |course|
+    if course.title
+      puts "Title: #{course.title}"
+      puts "  Schedule: #{course.schedule}"
+      puts "  Description: #{course.description}"
     end
   end
   
